e1bc73ceb6047eb6096636f3be31a2579b5db83f
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-06-18 18:52:16 +0500

    updated

--- src/routes/activity.routes.ts
+++ src/routes/activity.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getAllActivities, getActivityById } from "../controllers/activity.controller";
+import { getAllActivities, getActivitiesByUserId } from "../controllers/activity.controller";
 
 const router = Router();
 
 router.get("/getAllActivities", getAllActivities);
-router.get("/getActivitybyId/:id", getActivityById);
+router.get("/getActivitybyId/:id", getActivitiesByUserId);
 
 export default router;

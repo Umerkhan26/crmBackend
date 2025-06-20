0b54a1f30e36ab5cfeffa75bdce7b7f3349a0949
Umerkhan26 <114725165+Umerkhan26@users.noreply.github.com>
2025-06-20 20:04:00 +0500

    updated

--- src/queue/redisConnection.ts
+++ src/queue/redisConnection.ts
@@ -1,5 +1,14 @@
+// export const connection = {
+//     host: 'localhost',
+//     port: 6379,
+//   };
+//   //
+
+import dotenv from "dotenv";
+dotenv.config();
+
 export const connection = {
-    host: 'localhost',
-    port: 6379,
-  };
-  
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: Number(process.env.REDIS_PORT) || 6379,
+  // password: process.env.REDIS_PASSWORD, // Uncomment if needed
+};

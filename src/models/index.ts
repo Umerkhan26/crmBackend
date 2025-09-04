ec01d3de7bc7be29718984ceb5f006d862d477d6
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-09-04 23:46:53 +0500

    updated

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,16 +4,25 @@ import { associateModels } from "./relations";
 
 // Import all models (auto-registers them with Sequelize)
 import "./user.model";
-import "./chatmodels/message.model";
-import "./chatmodels/conversation.model";
-import "./chatmodels/conversationParticipant.model";
+// import "./chatmodels/message.model";
+// import "./chatmodels/conversation.model";
+// import "./chatmodels/conversationParticipant.model";
 
 import "./lead.model";
 
 import "./campaign.model";
-
+import "./clientLead.model";
+import "./leadActivity.model";
+import "./note.model";
 import "./activityLog.model";
 import "./notification.model"
 import "./emailLog.model";
+import "./order.model";
+import "./product.model";
+import "./role.model";
+import "./rolePermission.model";
+import "./permission.model";
+import "./emailTemplate.model";
+import "./emailPermission.model";
 // Apply associations
 associateModels();

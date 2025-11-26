24bc33d09202e66ed35a28539a4d711b3d6fba8f
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-11-26 23:32:31 +0500

    updated

--- src/routes/import.routes.ts
+++ src/routes/import.routes.ts
@@ -4,9 +4,15 @@ import { importClientLeadsController } from "../controllers/importClientLead.con
 import { verifyToken } from "../middleware/verifyToken.middleware";
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
-router.post("/client-leads/import", verifyToken, upload.single("file"), importClientLeadsController);
+const storage = multer.memoryStorage();
+
+export const upload = multer({
+    storage,
+    limits: {
+        fileSize: 20 * 1024 * 1024,  // allow up to 20MB file
+        fieldSize: 10 * 1024 * 1024, // allow up to 10MB text fields
+    },
+}); router.post("/client-leads/import", verifyToken, upload.single("file"), importClientLeadsController);
 
 
 export default router;
-   
